decd135ba46d73184d25253dae2c873516a98536
Gregory Derecho <gderecho@hmc.edu>
2015-07-19 00:04:17 -0400

    Add functions to manage text boxes
    Now all the blank text boxes are
    eliminated and new text boxes are
    automatically created

--- app/controllers/input_list.js
+++ app/controllers/input_list.js
@@ -1,11 +1,20 @@
 var inlist_module = angular.module("input_list", []);
 
 inlist_module.controller("input_list_ctrl", function() {
-   this.rows = [0,1,2];
-   this.check_if_add = function(index) {
-       if(index == this.rows.length-1) {
-           this.rows.push(this.rows.length)
+    this.inputs = [{id:1, text:""}];
+    this.curid = 1;
+    this.check_if_add = function(input) {
+        index = this.inputs.indexOf(input)
+        if(index == this.inputs.length-1) {
+            this.curid++;
+            this.inputs.push({id:this.curid, text:""})
        }
-   };
-   this.button_model=""
+    };
+    this.check_if_delete = function(input) {
+        index = this.inputs.indexOf(input)
+        if(index != this.inputs.length-1 &&
+                input.text == "") {
+            this.inputs.splice(index,1)
+        }
+    };
 });
