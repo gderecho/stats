512b7a02d52a5f09e7a88560bc91eba857cb9c92
Gregory Derecho <gdfusion@gmail.com>
2015-07-18 23:21:53 -0400

    Remove whitespace

--- app/controllers/input_list.js
+++ app/controllers/input_list.js
@@ -1,9 +1,6 @@
 var inlist_module = angular.module("input_list", []);
 
 inlist_module.controller("input_list_ctrl", function() {
-   this.qty = '1'; 
-   this.rows = ["hi","its","me"]; 
-        
-        
-        
+   this.qty = '1';
+   this.rows = ["hi","its","me"];
 });
