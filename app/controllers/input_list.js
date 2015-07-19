175e51a6d018daacb1d82b0e677a304f344c221b
Gregory Derecho <gdfusion@gmail.com>
2015-07-18 23:41:19 -0400

    Add weird expansion of inputs
    Now every time something is entered
    into the last input field, a new one
    is created. In addition, the content
    of all the input fields is synchron-
    ized.

--- app/controllers/input_list.js
+++ app/controllers/input_list.js
@@ -1,6 +1,11 @@
 var inlist_module = angular.module("input_list", []);
 
 inlist_module.controller("input_list_ctrl", function() {
-   this.qty = '1';
-   this.rows = ["hi","its","me"];
+   this.rows = [0,1,2];
+   this.check_if_add = function(index) {
+       if(index == this.rows.length-1) {
+           this.rows.push(this.rows.length)
+       }
+   };
+   this.button_model=""
 });
