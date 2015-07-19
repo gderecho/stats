f54cddba4bed4ba65c751c4cae58aa059205f92e
Gregory Derecho <gderecho@hmc.edu>
2015-07-19 00:16:47 -0400

    Add comments to js file, submit button to index

--- app/controllers/input_list.js
+++ app/controllers/input_list.js
@@ -3,6 +3,9 @@ var inlist_module = angular.module("input_list", []);
 inlist_module.controller("input_list_ctrl", function() {
     this.inputs = [{id:1, text:""}];
     this.curid = 1;
+
+    /* if there is no last blank input
+     * field, add a new one */
     this.check_if_add = function(input) {
         index = this.inputs.indexOf(input)
         if(index == this.inputs.length-1) {
@@ -10,6 +13,9 @@ inlist_module.controller("input_list_ctrl", function() {
             this.inputs.push({id:this.curid, text:""})
        }
     };
+
+    /* if, when unfocused, the element is blank
+     * but not the last element, remove it */
     this.check_if_delete = function(input) {
         index = this.inputs.indexOf(input)
         if(index != this.inputs.length-1 &&
