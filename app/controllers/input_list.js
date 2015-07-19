bdd7c5dff9e8dcc511aa42cfd5ef3389fd921e93
Gregory Derecho <gderecho@hmc.edu>
2015-07-18 23:19:50 -0400

    Set up basic structure (inital commit)

---
+++ app/controllers/input_list.js
@@ -0,0 +1,9 @@
+var inlist_module = angular.module("input_list", []);
+
+inlist_module.controller("input_list_ctrl", function() {
+   this.qty = '1'; 
+   this.rows = ["hi","its","me"]; 
+        
+        
+        
+});
