eb6479d86a7ea2e69c2e2796d7bee10aa227b93f
Gregory <gderecho@hmc.edu>
2015-07-23 19:22:53 -0400

    Fix #3 sanitize inputs to two prop z-test

--- app/controllers/hyptests_main.js
+++ app/controllers/hyptests_main.js
@@ -17,6 +17,10 @@ var hyptests_ctrl = hyptests.controller('choose_test_ctrl', function() {
  * 4.5.1. Taylor expansion of error function*/
 
 function erf(x) {
+    if(!x && x!=0)
+        throw "Bad erf input"
+    if(x==0)
+        return 0;
     if(x < 0)
         return -erf(-x);
     y=x*x;
@@ -50,10 +54,10 @@ var tpzt_ctrl =
         hyptests.controller('tpzt_controller',[ function() {
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
     this.nulldiff=0;
-    this.x1=0;
-    this.x2=0;
-    this.n1=0;
-    this.n2=0;
+    this.x1=undefined;
+    this.x2=undefined;
+    this.n1=undefined;
+    this.n2=undefined;
     this.alpha=0.05;
     this.bool_show=false;
     this.test_statistic=undefined;
@@ -62,6 +66,14 @@ var tpzt_ctrl =
     this.phat_2=undefined;
     this.p_value=undefined;
     this.show = function () {
+        if( !(this.x1
+            && this.x2
+            && this.n1
+            && this.n2
+            && this.n2>=this.x2
+            && this.n1>=this.x1) ) {
+            throw "Error: not all values detected";
+        }
         this.phat_1=this.x1/this.n1;
         this.phat_2=this.x2/this.n2;
         this.phat_pooled=(this.x1+this.x2)/(this.n1+this.n2)
@@ -79,5 +91,33 @@ hyptests.directive('gdTpztResults', function() {
     return {
         templateUrl: 'app/views/hyptests/tpzt_results_template.html'
     };
-}
-);
+});
+
+/* Makes sure that this
+ * is less than the denominator
+ */
+hyptests.directive('gdDenom', 
+        function() {
+    return {
+        scope:{
+            denom:'=gdDenom',
+        },
+        require:'ngModel',
+        link:function(scope,elem,attrs,ctrl) {
+            ctrl.$validators.gdDenom= function(thisval) {
+                console.log(thisval);
+                console.log(scope.denom);
+                a = parseInt(thisval) <= parseInt(scope.denom);
+                if(!a)
+                    //elem.attr('oninvalid',"this.setCustomValidity('Successes must be less than or equal to total observations.')");
+                    elem[0].setCustomValidity('Value must not be greater than number of observations.');
+                else
+                    elem[0].setCustomValidity('');
+                    //elem.removeAttr('oninvalid')
+                return a;
+            }
+            scope.$watch('denom', function(){ctrl.$validate()},true);
+        },
+    }
+});
+
